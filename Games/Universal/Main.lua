3e04b2d53afaf65240ac5384581d4f0b60e3feb2
onlyokok <126917421+onlyokok@users.noreply.github.com>
2024-08-08 22:06:29 +0100

    a

--- Games/Universal/Main.lua
+++ Games/Universal/Main.lua
@@ -11,9 +11,6 @@ return function(Package)
         ["MenuFadeTime"] = 0.1
     })
 
-    local Main = loadstring(game:HttpGet(`{Leaf}/Main.lua`))()
-    Main:Setup(Package, Window)
-
     local Visuals = loadstring(game:HttpGet(`{Leaf}/Visuals.lua`))()
     Visuals:Setup(Package, Window)
 
