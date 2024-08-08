fa2d691389a9d319eaae796f262bd4de79fbada5
onlyokok <126917421+onlyokok@users.noreply.github.com>
2024-08-08 21:04:31 +0100

    a

--- Games/Universal/Tabs/Main.lua
+++ Games/Universal/Tabs/Main.lua
@@ -9,191 +9,189 @@ function Main:Construct(Package)
         end
     end
 
-    local function MakeMovementSection(Side)
-        local Section = MakeSection("Movement", Side)
+    local Movement = MakeSection("movement", "Left")
 
-        Section:AddToggle("Fly", {
-            Text = "fly",
-            Default = false,
-        }):AddKeyPicker("FlyKeyPicker", {
-            Default = "None",
-            SyncToggleState = true,
-            Mode = "Toggle",
-            Text = "fly",
-            NoUI = false,
-        })
-    
-        Section:AddSlider("FlySpeed", {
-            Text = "fly speed",
-            Default = 50,
-            Min = 25,
-            Max = 200,
-            Rounding = 0,
-            Compact = false,
-        })
-    
-        Section:AddSlider("FallSpeed", {
-            Text = "fall speed",
-            Default = 25,
-            Min = 25,
-            Max = 200,
-            Rounding = 0,
-            Compact = false,
-        })
-    
-        Section:AddDivider()
-    
-        Section:AddToggle("WalkSpeed", {
-            Text = "walk speed",
-            Default = false,
-        }):AddKeyPicker('WalkSpeedKeyPicker', {
-            Default = 'None',
-            SyncToggleState = true,
-            Mode = 'Toggle',
-            Text = 'walk speed',
-            NoUI = false,
-        })
-    
-        Section:AddSlider('WalkSpeedValue', {
-            Text = 'value',
-            Default = 25,
-            Min = 5,
-            Max = 200,
-            Rounding = 0,
-            Compact = false,
-        })
-    
-        Section:AddDivider()
-    
-        Section:AddToggle("JumpPower", {
-            Text = "jump power",
-            Default = false,
-        }):AddKeyPicker("JumpPowerKeyPicker", {
-            Default = "None",
-            SyncToggleState = true,
-            Mode = "Toggle",
-            Text = "jump power",
-            NoUI = false,
-        })
-    
-        Section:AddSlider("JumpPowerValue", {
-            Text = "value",
-            Default = 50,
-            Min = 50,
-            Max = 200,
-            Rounding = 0,
-            Compact = false,
-        })
-    
-        Section:AddDropdown("JumpPowerMethod", {
-            Values = {"velocity", "humanoid"},
-            Default = 1,
-            Multi = false,
-            Text = "method",
-        })
-    
-        Section:AddDivider()
-    
-        Section:AddToggle("SafeMode", {
-            Text = "safe mode",
-            Default = false,
-        }):AddKeyPicker("SafeModeKeyPicker", {
-            Default = "None",
-            SyncToggleState = true,
-            Mode = "Toggle",
-            Text = "safe mode",
-            NoUI = false,
-        })
-    
-        Package.Interface.Linoria:GiveTask(task.spawn(function()
-            while task.wait() do
-                if Toggles.Fly.Value then
-                    if not Toggles.SafeMode.Value then
-                        if game.Players.LocalPlayer.Character then
-                            local HumanoidRootPart = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-                            local Velocity = Vector3.new(0, -Options.FallSpeed.Value / 10 , 0)
-            
-                            if HumanoidRootPart then
-                                if not game:GetService("UserInputService"):GetFocusedTextBox() then
-                                    if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.W) then
-                                        Velocity += (workspace.CurrentCamera.CFrame.LookVector * Options.FlySpeed.Value)
-                                    end
-                            
-                                    if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.S) then
-                                        Velocity += (workspace.CurrentCamera.CFrame.LookVector * -Options.FlySpeed.Value)
-                                    end
-                            
-                                    if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.A) then
-                                        Velocity += (workspace.CurrentCamera.CFrame.RightVector * -Options.FlySpeed.Value)
-                                    end
-                            
-                                    if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.D) then
-                                        Velocity += (workspace.CurrentCamera.CFrame.RightVector * Options.FlySpeed.Value)
-                                    end
+    Movement:AddToggle("Fly", {
+        Text = "fly",
+        Default = false,
+    }):AddKeyPicker("FlyKeyPicker", {
+        Default = "None",
+        SyncToggleState = true,
+        Mode = "Toggle",
+        Text = "fly",
+        NoUI = false,
+    })
+
+    Movement:AddSlider("FlySpeed", {
+        Text = "fly speed",
+        Default = 50,
+        Min = 25,
+        Max = 200,
+        Rounding = 0,
+        Compact = false,
+    })
+
+    Movement:AddSlider("FallSpeed", {
+        Text = "fall speed",
+        Default = 25,
+        Min = 25,
+        Max = 200,
+        Rounding = 0,
+        Compact = false,
+    })
+
+    Movement:AddDivider()
+
+    Movement:AddToggle("WalkSpeed", {
+        Text = "walk speed",
+        Default = false,
+    }):AddKeyPicker('WalkSpeedKeyPicker', {
+        Default = 'None',
+        SyncToggleState = true,
+        Mode = 'Toggle',
+        Text = 'walk speed',
+        NoUI = false,
+    })
+
+    Movement:AddSlider('WalkSpeedValue', {
+        Text = 'value',
+        Default = 25,
+        Min = 5,
+        Max = 200,
+        Rounding = 0,
+        Compact = false,
+    })
+
+    Movement:AddDivider()
+
+    Movement:AddToggle("JumpPower", {
+        Text = "jump power",
+        Default = false,
+    }):AddKeyPicker("JumpPowerKeyPicker", {
+        Default = "None",
+        SyncToggleState = true,
+        Mode = "Toggle",
+        Text = "jump power",
+        NoUI = false,
+    })
+
+    Movement:AddSlider("JumpPowerValue", {
+        Text = "value",
+        Default = 50,
+        Min = 50,
+        Max = 200,
+        Rounding = 0,
+        Compact = false,
+    })
+
+    Movement:AddDropdown("JumpPowerMethod", {
+        Values = {"velocity", "humanoid"},
+        Default = 1,
+        Multi = false,
+        Text = "method",
+    })
+
+    Movement:AddDivider()
+
+    Movement:AddToggle("SafeMode", {
+        Text = "safe mode",
+        Default = false,
+    }):AddKeyPicker("SafeModeKeyPicker", {
+        Default = "None",
+        SyncToggleState = true,
+        Mode = "Toggle",
+        Text = "safe mode",
+        NoUI = false,
+    })
+
+    Package.Interface.Linoria:GiveTask(task.spawn(function()
+        while task.wait() do
+            if Toggles.Fly.Value then
+                if not Toggles.SafeMode.Value then
+                    if game.Players.LocalPlayer.Character then
+                        local HumanoidRootPart = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+                        local Velocity = Vector3.new(0, -Options.FallSpeed.Value / 10 , 0)
         
-                                    HumanoidRootPart.Velocity = Velocity
+                        if HumanoidRootPart then
+                            if not game:GetService("UserInputService"):GetFocusedTextBox() then
+                                if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.W) then
+                                    Velocity += (workspace.CurrentCamera.CFrame.LookVector * Options.FlySpeed.Value)
                                 end
+                        
+                                if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.S) then
+                                    Velocity += (workspace.CurrentCamera.CFrame.LookVector * -Options.FlySpeed.Value)
+                                end
+                        
+                                if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.A) then
+                                    Velocity += (workspace.CurrentCamera.CFrame.RightVector * -Options.FlySpeed.Value)
+                                end
+                        
+                                if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.D) then
+                                    Velocity += (workspace.CurrentCamera.CFrame.RightVector * Options.FlySpeed.Value)
+                                end
+    
+                                HumanoidRootPart.Velocity = Velocity
                             end
                         end
                     end
                 end
             end
-        end))
-    
-        Package.Interface.Linoria:GiveTask(task.spawn(function()
-            while task.wait() do
-                if Toggles.WalkSpeed.Value then
+        end
+    end))
+
+    Package.Interface.Linoria:GiveTask(task.spawn(function()
+        while task.wait() do
+            if Toggles.WalkSpeed.Value then
+                if not Toggles.SafeMode.Value and not Toggles.Fly.Value then
+                    pcall(function()
+                        game.Players.LocalPlayer.Character:PivotTo(game.Players.LocalPlayer.Character:GetPivot() + (game.Players.LocalPlayer.Character.Humanoid.MoveDirection * (Options.WalkSpeedValue.Value / 100)))
+                    end)
+                end
+            end
+        end
+    end))
+
+    Package.Interface.Linoria:GiveSignal(game:GetService("UserInputService").InputBegan:Connect(function(Input, GameProcessed)
+        if not GameProcessed then
+            if Input.KeyCode == Enum.KeyCode.Space then
+                if Toggles.JumpPower.Value then
                     if not Toggles.SafeMode.Value and not Toggles.Fly.Value then
-                        pcall(function()
-                            game.Players.LocalPlayer.Character:PivotTo(game.Players.LocalPlayer.Character:GetPivot() + (game.Players.LocalPlayer.Character.Humanoid.MoveDirection * (Options.WalkSpeedValue.Value / 100)))
-                        end)
-                    end
-                end
-            end
-        end))
-    
-        Package.Interface.Linoria:GiveSignal(game:GetService("UserInputService").InputBegan:Connect(function(Input, GameProcessed)
-            if not GameProcessed then
-                if Input.KeyCode == Enum.KeyCode.Space then
-                    if Toggles.JumpPower.Value then
-                        if not Toggles.SafeMode.Value and not Toggles.Fly.Value then
-                            local HumanoidRootPart = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-                            local Humanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
-    
-                            if Options.JumpPowerMethod.Value == "velocity" and HumanoidRootPart then
-                                HumanoidRootPart.Velocity = Vector3.new(0, Options.JumpPowerValue.Value, 0)
-                            elseif Options.JumpPowerMethod.Value == "humanoid" and Humanoid then
-                                Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-                            end
+                        local HumanoidRootPart = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+                        local Humanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+
+                        if Options.JumpPowerMethod.Value == "velocity" and HumanoidRootPart then
+                            HumanoidRootPart.Velocity = Vector3.new(0, Options.JumpPowerValue.Value, 0)
+                        elseif Options.JumpPowerMethod.Value == "humanoid" and Humanoid then
+                            Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
                         end
                     end
                 end
             end
-        end))
-    
-        Section:AddLabel("Teleport To Ground"):AddKeyPicker("TeleportToGroundKeyPicker", {
-            Default = "None",
-            SyncToggleState = true,
-            Mode = "Toggle",
-            Text = "teleport to ground",
-            NoUI = true,
-            Callback = function()
-                local Origin = game.Players.LocalPlayer.Character:GetPivot().Position
-                local Direction = Vector3.new(0, -9e9, 0)
-    
-                local Params = RaycastParams.new()
-                Params.FilterType = Enum.RaycastFilterType.Exclude
-                Params.FilterDescendantsInstances = game.Players.LocalPlayer.Character:GetDescendants()
-    
-                local Result = workspace:Raycast(Origin, Direction, Params)
-                game.Players.LocalPlayer.Character:PivotTo(CFrame.new(Result.Position))
-            end
-        })
+        end
+    end))
 
-        return Section
-    end
+    Movement:AddLabel("Teleport To Ground"):AddKeyPicker("TeleportToGroundKeyPicker", {
+        Default = "None",
+        SyncToggleState = true,
+        Mode = "Toggle",
+        Text = "teleport to ground",
+        NoUI = true,
+        Callback = function()
+            local Origin = game.Players.LocalPlayer.Character:GetPivot().Position
+            local Direction = Vector3.new(0, -9e9, 0)
 
-    MakeMovementSection("Left")
+            local Params = RaycastParams.new()
+            Params.FilterType = Enum.RaycastFilterType.Exclude
+            Params.FilterDescendantsInstances = game.Players.LocalPlayer.Character:GetDescendants()
+
+            local Result = workspace:Raycast(Origin, Direction, Params)
+            game.Players.LocalPlayer.Character:PivotTo(CFrame.new(Result.Position))
+        end
+    })
+
+    local Aimbot = MakeSection("Aimbot", "Right")
+
+    
 end
 
 function Main:Setup(Package, Window)
--- Games/Universal/Tabs/Visuals.lua
+++ Games/Universal/Tabs/Visuals.lua
@@ -9,160 +9,170 @@ function Visuals:Construct(Package)
         end
     end
 
-    local function MakeOptionsSection(Side)
-        local Section = MakeSection("options", Side)
+    local Options = MakeSection("options", Side)
 
-        Section:AddToggle('EspEnabled', {
-            Text = 'enabled',
-            Default = true,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.Enabled = Value
-            end
-        }):AddKeyPicker("EspEnabledKeyPicker", {
-            Default = "None",
-            SyncToggleState = true,
-            Mode = "Toggle",
-            NoUI = true,
-        })
-    
-        Section:AddToggle('EspLimitDistance', {
-            Text = 'limit distance',
-            Default = false,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.LimitDistance = Value
-            end
-        })
-    
-        Section:AddSlider("EspMaxDistance", {
-            Text = "max distance",
-            Default = 20000,
-            Min = 0,
-            Max = 20000,
-            Rounding = 0,
-            Compact = false,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.MaxDistance = Value
-            end
-        })
-    
-        Section:AddDivider()
-    
-        Section:AddToggle('EspCheckTeam', {
-            Text = 'check team',
-            Default = false,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.CheckTeam = Value
-            end
-        })
-    
-        Section:AddToggle('EspUseTeamColor', {
-            Text = 'use team color',
-            Default = false,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.UseTeamColor = Value
-            end
-        })
-    
-        Section:AddDivider()
-    
-        local Fonts = {
-            UI = 0,
-            System = 1,
-            Plex = 2,
-            Monospace = 3
-        }
-    
-        Section:AddDropdown("EspTextFont", {
-            Values = {"UI", "System", "Plex", "Monospace"},
-            Default = 4,
-            Multi = false,
-            Text = "text font",
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.TextFont = Fonts[Value]
-            end
-        })
-    
-        Section:AddSlider("EspTextSize", {
-            Text = "text size",
-            Default = 12,
-            Min = 10,
-            Max = 20,
-            Rounding = 0,
-            Compact = false,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.TextSize = Value
-            end
-        })
+    Options:AddToggle('EspEnabled', {
+        Text = 'enabled',
+        Default = true,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.Enabled = Value
+        end
+    }):AddKeyPicker("EspEnabledKeyPicker", {
+        Default = "None",
+        SyncToggleState = true,
+        Mode = "Toggle",
+        NoUI = true,
+    })
 
-        return Section
-    end
+    Options:AddToggle('EspLimitDistance', {
+        Text = 'limit distance',
+        Default = false,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.LimitDistance = Value
+        end
+    })
 
-    local function MakePlayersSection(Side)
-        local Section = MakeSection("players", Side)
+    Options:AddSlider("EspMaxDistance", {
+        Text = "max distance",
+        Default = 20000,
+        Min = 0,
+        Max = 20000,
+        Rounding = 0,
+        Compact = false,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.MaxDistance = Value
+        end
+    })
 
-        Section:AddToggle("EspName", {
-            Text = "name",
-            Default = true,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.Name = Value
-            end
-        }):AddColorPicker("EspNameColorPicker", {
-            Default = Color3.new(1, 1, 1),
-            Title = "color",
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.NameColor = Value
-            end
-        })
-    
-        Section:AddToggle('EspDistance', {
-            Text = 'distance',
-            Default = true,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.ShowDistance = Value
-            end
-        })
-    
-        Section:AddToggle('EspBox', {
-            Text = 'box',
-            Default = true,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.Box = Value
-            end
-        }):AddColorPicker("EspBoxColorPicker", {
-            Default = Color3.new(1, 1, 1),
-            Title = "color",
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.BoxColor = Value
-            end
-        })
-    
-        Section:AddToggle('EspHealthBar', {
-            Text = 'health bar',
-            Default = true,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.HealthBar = Value
-            end
-        })
-    
-        Section:AddToggle('EspHealthText', {
-            Text = 'health text',
-            Default = true,
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.HealthText = Value
-            end
-        }):AddColorPicker("EspHealthColorPicker", {
-            Default = Color3.fromRGB(0, 255, 0),
-            Title = "color",
-            Callback = function(Value)
-                Package.Helpers.Esp.Settings.HealthTextColor = Value
-            end
-        })
+    Options:AddDivider()
 
-        return Section
-    end
+    Options:AddToggle('EspCheckTeam', {
+        Text = 'check team',
+        Default = false,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.CheckTeam = Value
+        end
+    })
 
-    MakeOptionsSection("Left")
-    MakePlayersSection("Right")
+    Options:AddToggle('EspUseTeamColor', {
+        Text = 'use team color',
+        Default = false,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.UseTeamColor = Value
+        end
+    })
+
+    Options:AddDivider()
+
+    local Fonts = {
+        UI = 0,
+        System = 1,
+        Plex = 2,
+        Monospace = 3
+    }
+
+    Options:AddDropdown("EspTextFont", {
+        Values = {"UI", "System", "Plex", "Monospace"},
+        Default = 4,
+        Multi = false,
+        Text = "text font",
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.TextFont = Fonts[Value]
+        end
+    })
+
+    Options:AddSlider("EspTextSize", {
+        Text = "text size",
+        Default = 12,
+        Min = 10,
+        Max = 20,
+        Rounding = 0,
+        Compact = false,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.TextSize = Value
+        end
+    })
+
+    local Players = MakeSection("players", Side)
+
+    Players:AddToggle("EspName", {
+        Text = "name",
+        Default = true,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.Name = Value
+        end
+    }):AddColorPicker("EspNameColorPicker", {
+        Default = Color3.new(1, 1, 1),
+        Title = "color",
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.NameColor = Value
+        end
+    })
+
+    Players:AddToggle('EspDistance', {
+        Text = 'distance',
+        Default = true,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.ShowDistance = Value
+        end
+    })
+
+    Players:AddToggle('EspBox', {
+        Text = 'box',
+        Default = true,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.Box = Value
+        end
+    }):AddColorPicker("EspBoxColorPicker", {
+        Default = Color3.new(1, 1, 1),
+        Title = "color",
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.BoxColor = Value
+        end
+    })
+
+    Players:AddToggle('EspHealthBar', {
+        Text = 'health bar',
+        Default = true,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.HealthBar = Value
+        end
+    })
+
+    Players:AddToggle('EspHealthText', {
+        Text = 'health text',
+        Default = true,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.HealthText = Value
+        end
+    }):AddColorPicker("EspHealthColorPicker", {
+        Default = Color3.fromRGB(0, 255, 0),
+        Title = "color",
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.HealthTextColor = Value
+        end
+    })
+
+    local World = MakeSection("world", "Right")
+
+    World:AddToggle('Ambient', {
+        Text = 'ambient',
+        Default = true,
+        Callback = function(Value)
+            Package.Helpers.Esp.Settings.HealthText = Value
+        end
+    }):AddColorPicker("AmbientColorPicker", {
+        Default = Color3.fromRGB(255, 255, 255),
+        Title = "color",
+    })
+
+    local ColorCorrectionEffect = Instance.new("ColorCorrectionEffect", game.Lighting)
+
+    Package.Interface.Linoria:GiveTask(task.spawn(function()
+        while task.wait(0.01) do
+            ColorCorrectionEffect.TintColor = Toggles.Ambient.Value and Options.AmbientColorPicker.Value or Color3.fromRGB(255, 255, 255)
+        end
+    end))
 
     for _,Player in next, game.Players:GetPlayers() do
         if Player ~= game.Players.LocalPlayer then
