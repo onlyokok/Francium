a55d0b30365a454082524a3036d41e077ed80fcd
onlyokok <126917421+onlyokok@users.noreply.github.com>
2024-08-10 17:48:33 +0100

    a

--- Games/7353845952/Tabs/Visuals.lua
+++ Games/7353845952/Tabs/Visuals.lua
@@ -172,7 +172,7 @@ function Visuals:Construct(Package)
         Default = false,
     }):AddKeyPicker('KeyPicker', {
         Default = 'None',
-        SyncToggleState = true,
+        SyncToggleState = false,
         Mode = 'Hold',
         NoUI = true,
     })
